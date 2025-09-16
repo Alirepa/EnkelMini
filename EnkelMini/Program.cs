04c793752b0c2598409c1d76a84a13287de85470
Ali <panahi.ali0606@gmail.com>
2025-09-17 00:00:24 +0300

    "Skapat meny för miniräknaren"

--- EnkelMini/Program.cs
+++ EnkelMini/Program.cs
@@ -1,3 +1,18 @@
 ﻿using System;
 
 
+class Program
+{
+    static void Main(string[] args) { 
+        Console.WriteLine("Välkommen till EnkelMini!");
+        Console.WriteLine("1 för addition");
+        Console.WriteLine("2 för subtraktion");
+        Console.WriteLine("3 för multiplikation");
+        Console.WriteLine("4 för division");
+        
+
+        string choice = Console.ReadLine();
+    }
+    
+    
+}
