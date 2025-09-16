5a31c84b18e175393dc4f52a78bbb8e68f1fd957
Ali <panahi.ali0606@gmail.com>
2025-09-17 00:28:00 +0300

    "Lagt till metoder med flera parameter för räknesätten"

--- EnkelMini/Program.cs
+++ EnkelMini/Program.cs
@@ -9,10 +9,35 @@ class Program
         Console.WriteLine("2 för subtraktion");
         Console.WriteLine("3 för multiplikation");
         Console.WriteLine("4 för division");
-        
+       
+        int choice = Convert.ToInt32(Console.ReadLine());
 
-        string choice = Console.ReadLine();
+        Console.WriteLine("Ange första talet:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ange andra talet:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
     }
     
-    
+
+    static void Addition(double num1, double num2)
+    {
+        var sum = num1 + num2;
+        Console.WriteLine($"Addition sum: {sum}");
+    } 
+    static void Subtraction(double num1, double num2)
+    {
+        var sum = num1 - num2;
+        Console.WriteLine($"Subtractino sum: {sum}");
+    }
+    static void Multiplication(double num1, double num2)
+    {
+        var sum = num1 * num2;
+        Console.WriteLine($"Multiplication sum: {sum}");
+    }
+    static void Division(double num1, double num2)
+    {
+        var sum = num1 / num2;
+        Console.WriteLine($"Division sum: {sum}");
+    }
 }
